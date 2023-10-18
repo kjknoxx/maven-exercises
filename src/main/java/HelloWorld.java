0fbd3ab81c89c1119a2d167d11ed5298c74ac69e
Kaylee Knox <kjknoxx90@gmail.com>
2023-10-18 14:44:45 -0500

    This was easy.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class HelloWorld {
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Give me a string.");
+        String input = src.nextLine();
+        System.out.println(StringUtils.capitalize(input));
+        System.out.println(StringUtils.reverse(input));
+        System.out.println("Is your string Numeric? "+StringUtils.isNumeric(input));
+        System.out.println(StringUtils.swapCase(input));
+    }
+
 }
